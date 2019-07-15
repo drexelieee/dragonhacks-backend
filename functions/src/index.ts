58c8d10c7ab7b5f022cc00ea5f42ceef181af8b9
Jason R. Carrete <jasoncarrete5@gmail.com>
2019-07-15 11:20:14 -0400

    Change npm install to npm ci in .travis.yml

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
 
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //   response.send("Hello from Firebase!");
