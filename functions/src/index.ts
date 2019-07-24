a34d534f944c34cb33ba165a1e1d3e2fd5d8b324
Zach <zachmatuson@gmail.com>
2019-07-24 00:12:57 -0400

    Created submitSponserForm function
    emails teamdragonhacks@gmail.com with sponsorship request

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,50 @@
 import * as functions from 'firebase-functions';
+import * as nodemailer from "nodemailer";
+import * as admin from "firebase-admin";
+import { MailOptions } from 'nodemailer/lib/json-transport';
+import { emailAuth } from './.secret/emailAuth';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+admin.initializeApp();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: emailAuth
+});
+
+/**
+ * Generates an email for a sponsorship request.
+ * @returns {boolean} indicates success
+ */
+export const submitSponserForm = functions.https.onRequest(async (request, response) => {
+  // ignore non posts
+  if (request.method !== "POST") {
+    response.send(false);
+  }
+
+  let text = `I am interested in the ${request.body.package} package.`;
+
+  if (request.body.donation > 0) {
+    text += `\nAdditionally, I would like to donate $${request.body.donation}.`;
+  }
+
+  text += `\n\n${request.body.message}
+
+You can contact me at ${request.body.contactEmail}
+
+From,
+    ${request.body.contactName}`;
+
+  const mailOptions: MailOptions = {
+    from: "teamdragonhacks@gmail.com",
+    to: "teamdragonhacks@gmail.com",
+    subject: `New sponsorship request from ${request.body.organization}`,
+    text: text
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    response.send(true);
+  } catch (e) {
+    response.send(e);
+  }
 });
